87e5567aedb8d2e40266ef4b84fb6303b9fb31f1
Yuriy Epikhin <epih96@mail.ru>
2023-11-04 03:56:36 +0300

    git init

---
+++ resources/js/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="text-red-700">Привет, {{ username }}!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    components: {},
+    setup() {
+        return {
+            username: 'Михаил'
+        };
+    },
+});
+</script>
+
+
+<style lang="less" scoped>
+    
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.{html,js}",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+      ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
